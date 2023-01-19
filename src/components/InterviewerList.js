eb4960b78367b4a2c0a78cb9ecbbce217976930f
JUNTAO LIU <106684251+juntaodev@users.noreply.github.com>
2023-01-19 16:20:04 -0500

    remove terminal warnings

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import classNames from "classnames";
+
 import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 import PropTypes from "prop-types";
